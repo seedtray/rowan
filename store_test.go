c03686496e801594e2bee01bee2520c2fd047efa
Julian Gutierrez Oschmann <julian.gutierrez.o@gmail.com>
2019-08-27 11:40:20 -0700

    First iteration on a basic HTTP relay server. (#1)
    * First iteration on a basic HTTP relay server.
    
    Mostly functional. Lot of flags and parameters to add and wire up.
    The main machinery is made of three concurrent cooperating components (namely, an HTTP server, a master, N workers and a durable store).
    
    The HTTP server persist incoming request (for durability) after some basic validation / normalization.
    
    A master component is in charge of scanning and scheduling requests whereas workers are merely responsible for request delivery (ie. executing an HTTP call to the target endpoint).
    
    The store is based on BoltDB, although we can certainly revisit this later.
    
    * Some changes after Rafa's comment.
    
    * more fixes

---
+++ store_test.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+)
+
+type testStore struct {
+	tmpFilePath string
+	db          *BoltRequestStore
+}
+
+func newTestStore() (*testStore, error) {
+	var t testStore
+	tmpFile, err := ioutil.TempFile("", "tmp_store")
+	if err != nil {
+		return &t, err
+	}
+	t.tmpFilePath = tmpFile.Name()
+	db, err := newBoltRequestStore(tmpFile.Name())
+	if err != nil {
+		return &t, err
+	}
+	t.db = db
+	return &t, nil
+}
+
+func (t *testStore) tearDown() {
+	if t.tmpFilePath != "" {
+		os.Remove(t.tmpFilePath)
+	}
+}
+
+func TestPut(t *testing.T) {
+	s, err := newTestStore()
+	defer s.tearDown()
+	if err != nil {
+		t.Fatalf("Could not create test store: %v", err)
+	}
+
+	err = s.db.Put(&StoredRequest{
+		UID:          []byte{1},
+		DeliveryTime: 1,
+		Path:         "/foo",
+		Method:       "GET",
+	})
+	if err != nil {
+		t.Fatalf("Put failed: %v", err)
+	}
+
+	all, err := s.db.Next(1)
+	if err != nil {
+		t.Fatalf("Failed to Scan requests: %v", err)
+	}
+
+	if len(all) != 1 {
+		t.Fatalf("Invalid number of requests. Expexted 1 but got %d", len(all))
+	}
+
+	actual := all[0]
+	if actual.DeliveryTime != 1 {
+		t.Fatalf("Wrong delivery time. Expected 1 but got %d", actual.DeliveryTime)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	s, err := newTestStore()
+	defer s.tearDown()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req1 := &StoredRequest{
+		UID:          []byte{1},
+		DeliveryTime: 100,
+		Path:         "/foo",
+		Method:       "GET",
+	}
+
+	req2 := &StoredRequest{
+		UID:          []byte{2},
+		DeliveryTime: 223,
+		Path:         "/bar",
+		Method:       "POST",
+	}
+
+	for _, r := range []*StoredRequest{req2, req1} {
+		if err = s.db.Put(r); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	if err = s.db.Delete(req2); err != nil {
+		t.Fatal(err)
+	}
+
+	want := []*StoredRequest{req1}
+	got, err := s.db.Next(2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got requests %v, want %v", got, want)
+	}
+
+}
+
+func TestNext(t *testing.T) {
+	s, err := newTestStore()
+	defer s.tearDown()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req1 := &StoredRequest{
+		UID:          []byte{1},
+		DeliveryTime: 100,
+		Path:         "foo",
+		Method:       "GET",
+	}
+
+	req2 := &StoredRequest{
+		UID:          []byte{2},
+		DeliveryTime: 223,
+		Path:         "bar",
+		Method:       "POST",
+		Scheduled:		true,
+	}
+
+	req3 := &StoredRequest{
+		UID:          []byte{3},
+		DeliveryTime: 321,
+		Path:         "baz",
+		Method:       "GET",
+	}
+
+	for _, r := range []*StoredRequest{req2, req3, req1} {
+		if err = s.db.Put(r); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	want := []*StoredRequest{req1}
+	got, err := s.db.Next(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got requests %v, want %v", got, want)
+	}
+
+	want = []*StoredRequest{req1, req3}
+	got, err = s.db.Next(2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got requests %v, want %v", got, want)
+	}
+
+	want = []*StoredRequest{req1, req3}
+	got, err = s.db.Next(3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got requests %v, want %v", got, want)
+	}
+}
---
+++ worker.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type Worker struct {
+	id        int
+	requests  chan *StoredRequest
+	serverURL *url.URL
+	client    *http.Client
+	store     RequestStore
+}
+
+func newWorker(id int,
+	requests chan *StoredRequest,
+	serverURL *url.URL,
+	client *http.Client,
+	store RequestStore) *Worker {
+	return &Worker{
+		id:        id,
+		requests:  requests,
+		serverURL: serverURL,
+		client:    client,
+		store:     store,
+	}
+}
+
+func (w *Worker) run(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case r := <-w.requests:
+			w.send(r)
+		}
+	}
+}
+
+func (w *Worker) send(s *StoredRequest) {
+	url, err := url.Parse(w.serverURL.String())
+	if err != nil {
+		log.Printf("Could not parse URL: %v", err)
+		w.reschedule(s)
+	}
+	url.Path = s.Path
+	req, err := http.NewRequest(s.Method, url.String(), nil)
+	if err != nil {
+		log.Fatalf("Could not create request: %v", s)
+	}
+	res, err := w.client.Do(req)
+	if err != nil {
+		log.Printf("Could not get response: %v", err)
+		w.reschedule(s)
+		return
+	}
+	if res.StatusCode != 200 {
+		log.Printf("Bad response. Request: %v. Status:: %d", s, res.StatusCode)
+		w.reschedule(s)
+		return
+	}
+	err = w.store.Delete(s)
+	if err != nil {
+		log.Fatalf("Could not delete stored request: %v", err)
+	}
+}
+
+func (w *Worker) reschedule(s *StoredRequest) {
+	if s.TTL == 1 {
+		log.Printf("Request expired: %v\n", s)
+		err := w.store.Delete(s)
+		if err != nil {
+			log.Fatalf("Could not delete request: %v", err)
+		}
+		return
+	}
+
+	rescheduled := &StoredRequest{
+		UID: s.UID,
+		// TODO: Use a parameterized exponential backoff
+		DeliveryTime: time.Now().Add(10 * time.Second).UnixNano(),
+		Path:         s.Path,
+		Method:       s.Method,
+		Scheduled:    false,
+	}
+
+	if s.TTL > 1 {
+		rescheduled.TTL = s.TTL - 1
+	}
+
+	err := w.store.Reschedule(s, rescheduled)
+	if err != nil {
+		log.Fatalf("Could not store rescheduled request: %v", err)
+	}
+}
